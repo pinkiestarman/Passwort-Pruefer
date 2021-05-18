1ac8de9611b1efe74dda452f21fd5ab19abbeca3
Pinkie Starman <jansteenken128@gmail.com>
2021-05-18 11:04:43 +0200

    Erste Funktionen

---
+++ pypassproof.py
@@ -0,0 +1,33 @@
+#! python3
+
+def passlen(minlen = 8, password=""):
+    """
+    Ist das Passwort lang genug?
+    @return bool
+    """
+    return password.length >= minlen
+
+def howmanynumbers(password=""):
+    """
+    Wie viele Zahlen sind im string
+    @return int
+    """
+    r = 0
+    for c in password:
+        r += c.isdecimal()
+    return r
+
+def howmanyspecial(string = ""):
+    """
+    Wie viele Sonderzeichen sind im string
+    @return int
+    """
+    r = 0
+    for c in string:
+        r += c.isalnum()
+    return r - string.length
+
+def readcfg(file = "passwdparams.txt"):
+    f = open(file)
+    paramlist = f.read().splitlines()
+    print(paramlist)
