bf41ce8fd28a6a12e5b01d1d68dce70d1581b33e
Pinkie Starman <jansteenken128@gmail.com>
2021-06-15 12:06:46 +0200

    passcriteria

--- einaus.py
+++ einaus.py
@@ -11,4 +11,6 @@ def ausgabe(istgut = False):
 
 def passcriteriain():
     passcriteria = {"Length":8,"Upper":1,"Lower":1,"Numbers":1,"Special":1}
+    # hier muss ne eingabe hin
+    passcriteria["Length"] = int(input("Wie lang soll das Passwort sein?: "))
     return passcriteria
